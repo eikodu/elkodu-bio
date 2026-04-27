bc6026b78154e65cb18eda40ff274d77508413b5
eikodu <eiko3642@gmail.com>
2026-04-27 19:08:54 +0400

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,21 +1,21 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'mytiny.page',
-        url: 'https://mytiny.page',
+        name: 'Elko',
+        username: 'Elko',
+        url: 'https://elkodu.org',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'Hello. ',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
+        subdesc: 'Welcome',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
-        author: 'realvjy',
+        author: 'Elko',
         authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
+        footerText: 'made by elko'
     },
 ];
 
