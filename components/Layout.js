99492e8c072797fc69cefe967323701a61d620de
eikodu <eiko3642@gmail.com>
2026-04-27 21:29:18 +0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,44 @@
 import styled from 'styled-components'
 import ReactDOM from 'react-dom';
 
-
-
 export default function Layout({ children }) {
-    return (
-        <Main >
-            {children}
-        </Main>
-    )
+  return (
+    <Main>
+      {/* Video Fonu */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: 0,
+          filter: 'brightness(0.4)' // Videonu qaraldır ki, yazılar aydın görünsün
+        }}
+      >
+        <source src="/background.mp4" type="video/mp4" />
+      </video>
+
+      {/* Saytın məzmunu (linklər, Elko yazısı və s.) */}
+      <ContentWrapper>
+        {children}
+      </ContentWrapper>
+    </Main>
+  )
 }
 
 const Main = styled.main`
   min-height: 100vh;
-  background: url('/bg.png') no-repeat;
-  background-size: 100%;
-  background-position: -50vh 10%;
-  background-attachment: fixed;
-  @media screen and (max-width: ${({ theme }) => theme.deviceSize.tablet}) {
-    background-size: 250%;
-    background-position: -50vh 30vh;
-   }
+  position: relative;
+  background: #000; /* Video yüklənənə qədər qara fon görünsün */
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  z-index: 1; /* Yazıların videonun üstündə qalmasını təmin edir */
 `;
